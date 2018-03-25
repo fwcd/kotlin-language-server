afdca01005b8c53b66d3b26559779f21cf2d0076
George Fraser <george@fivetran.com>
2018-03-24 18:46:32 -0700

    Incomplete call

--- src/test/kotlin/org/javacs/kt/ResolveTest.kt
+++ src/test/kotlin/org/javacs/kt/ResolveTest.kt
@@ -1,8 +1,10 @@
 package org.javacs.kt
 
-import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.*
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
+import org.jetbrains.kotlin.resolve.calls.callUtil.getParentCall
 import org.jetbrains.kotlin.resolve.calls.callUtil.getParentResolvedCall
+import org.jetbrains.kotlin.resolve.scopes.receivers.QualifierReceiver
 import org.junit.Assert.assertThat
 import org.junit.Test
 
@@ -37,5 +39,22 @@ object Bar {
         assertThat(call.candidateDescriptor.name.asString(), equalTo("foo"))
         assertThat(call.candidateDescriptor.findPsi()!!.containingFile.name, equalTo(testFileName))
         assertThat(call.candidateDescriptor.containingDeclaration.name.asString(), equalTo("Foo"))
+    }
+
+    @Test
+    fun `resolve an incomplete member`() {
+        val text = """
+object Foo {
+    fun foo() = "Foo"
+}
+object Bar {
+    fun bar() = Foo.fo
+}"""
+        val (file, analyze) = parseAnalyze(text)
+        val foo = findExpressionAt(file, 72)!!
+        val call = foo.getParentCall(analyze.bindingContext, false)!!
+        val receiver = call.explicitReceiver as QualifierReceiver
+
+        assertThat(receiver.staticScope.getFunctionNames(), hasItem(hasToString("foo")))
     }
 }
