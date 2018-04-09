ca87239271257912011874ea90855e9faf225c78
George Fraser <george@fivetran.com>
2018-04-08 22:26:03 -0700

    Test findClassPath

---
+++ src/test/kotlin/org/javacs/kt/ClassPathTest.kt
@@ -0,0 +1,21 @@
+package org.javacs.kt
+
+import org.hamcrest.Matchers.*
+import org.javacs.kt.classpath.findClassPath
+import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.nio.file.Files
+
+class ClassPathTest {
+    @Test fun findMavenClassPath() {
+        val workspaceRoot = testResourcesRoot().parent.parent
+        val pom = workspaceRoot.resolve("pom.xml")
+
+        assertTrue(Files.exists(pom))
+
+        val classPath = findClassPath(listOf(workspaceRoot))
+
+        assertThat(classPath, hasItem(hasToString(containsString("guava"))))
+    }
+}
