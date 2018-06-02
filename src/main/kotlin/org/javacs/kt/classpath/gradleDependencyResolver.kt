97b0282e69145788a8099bad3ed6b333c3958e3e
Donnie West <me@donniewest.com>
2018-06-01 22:56:19 -0500

    Initial support for Android projects

--- src/main/kotlin/org/javacs/kt/classpath/gradleDependencyResolver.kt
+++ src/main/kotlin/org/javacs/kt/classpath/gradleDependencyResolver.kt
@@ -1,7 +1,11 @@
 package org.javacs.kt.classpath
 
 import org.javacs.kt.LOG
+import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.regex.Pattern
 import org.gradle.tooling.*
 import org.gradle.tooling.model.*
 import org.gradle.tooling.model.eclipse.*
@@ -16,21 +20,162 @@ fun readBuildGradle(buildFile: Path): Set<Path> {
     var dependencies: Set<Path> = emptySet()
 
     try {
-        dependencies = readDependenciesViaEclipseProject(connection)
-    } catch (e: BuildException) {
+        dependencies = readDependenciesViaTask(projectDirectory.toPath())
+    } catch(e: BuildException) {
         try {
-            dependencies = readDependenciesViaKotlinDSL(connection)
+            dependencies = readDependenciesViaEclipseProject(connection)
         } catch (f: BuildException) {
             try {
-                dependencies = readDependenciesViaIdeaProject(connection)
+                dependencies = readDependenciesViaKotlinDSL(connection)
             } catch (g: BuildException) {
-                LOG.warning("BuildExceptions while collecting Gradle dependencies: ${e.message} and ${f.message} and ${g.message}")
+                try {
+                    dependencies = readDependenciesViaIdeaProject(connection)
+                } catch (h: BuildException) {
+                    LOG.warning("BuildExceptions while collecting Gradle dependencies: ${e.message} and ${f.message} and ${g.message} and ${h.message}")
+                }
             }
         }
     }
 
+
     connection.close()
     return dependencies
+}
+
+
+private fun createTemporaryGradleFile(): File {
+  val temp = File.createTempFile("tempGradle", ".config")
+  val config = File.createTempFile("classpath", ".gradle")
+  val classPathGradleConfig = """
+    boolean isResolvable(Configuration conf) {
+      // isCanBeResolved was added in Gradle 3.3. Previously, all configurations were resolvable
+      if (Configuration.class.declaredMethods.any { it.name == 'isCanBeResolved' }) {
+        return conf.canBeResolved
+      }
+      return true
+    }
+
+    File getBuildCacheForDependency(File dependency) {
+      String name = dependency.getName()
+      String home = System.getProperty("user.home")
+      String gradleCache = home + File.separator + '.gradle' + File.separator + 'caches' + File.separator
+      if (file(gradleCache).exists()) {
+        String include = 'transforms*' + File.separator + '**' + File.separator + name + File.separator + '**' + File.separator + 'classes.jar'
+        return fileTree(dir: gradleCache, include: include).files.find { it.isFile() }
+      } else {
+        return zipTree(dependency).files.find { it.isFile() && it.name.endsWith('jar') }
+      }
+    }
+
+    task classpath {
+      doLast {
+        HashSet<String> classpathFiles = new HashSet<String>()
+        for (proj in allprojects) {
+          for (conf in proj.configurations) {
+            if (isResolvable(conf)) {
+              for (dependency in conf) {
+                classpathFiles += dependency
+              }
+            }
+          }
+
+          def rjava = proj.getBuildDir().absolutePath + File.separator + "intermediates" + File.separator + "classes" + File.separator + "debug"
+          def rFiles = new File(rjava)
+          if (rFiles.exists()) {
+            classpathFiles += rFiles
+          }
+
+          if (proj.hasProperty("android")) {
+            classpathFiles += proj.android.getBootClasspath()
+            if (proj.android.hasProperty("applicationVariants")) {
+              proj.android.applicationVariants.all { v ->
+                if (v.hasProperty("javaCompile")) {
+                  classpathFiles += v.javaCompile.classpath
+                }
+                if (v.hasProperty("compileConfiguration")) {
+                  v.compileConfiguration.each { dependency ->
+                    classpathFiles += dependency
+                  }
+                }
+                if (v.hasProperty("runtimeConfiguration")) {
+                  v.runtimeConfiguration.each { dependency ->
+                    classpathFiles += dependency
+                  }
+                }
+                if (v.hasProperty("getApkLibraries")) {
+                  println v.getApkLibraries()
+                  classpathFiles += v.getApkLibraries()
+                }
+                if (v.hasProperty("getCompileLibraries")) {
+                  classpathFiles += v.getCompileLibraries()
+                }
+              }
+            }
+
+            if (proj.android.hasProperty("libraryVariants")) {
+              proj.android.libraryVariants.all { v ->
+                classpathFiles += v.javaCompile.classpath.files
+              }
+            }
+          }
+
+        HashSet<String> computedPaths = new HashSet<String>()
+        for (dependency in classpathFiles) {
+          if (dependency.name.endsWith("jar")) {
+            println dependency
+          } else if (dependency != null) {
+            println getBuildCacheForDependency(dependency)
+          }
+        }
+
+      }
+    }
+  }
+  """
+  val bw = config.bufferedWriter()
+  bw.write(classPathGradleConfig)
+  bw.close()
+
+  val tempWriter = temp.bufferedWriter()
+  tempWriter.write(String.format("rootProject{ apply from: '%s'} ", config.getAbsolutePath()));
+  tempWriter.close();
+
+  return temp;
+}
+
+private var cacheGradleCommand: Path? = null
+
+private fun getGradleCommand(workspace: Path): Path {
+  if (cacheGradleCommand == null)
+    cacheGradleCommand = workspace.resolve("gradlew").toAbsolutePath()
+
+  return workspace.resolve("gradlew").toAbsolutePath()
+}
+
+private fun readDependenciesViaTask(directory: Path): Set<Path> {
+  val gradle = getGradleCommand(directory)
+  if (!gradle.toFile().exists()) return mutableSetOf<Path>()
+  val config = createTemporaryGradleFile()
+
+  val gradleCommand = String.format("%s -I %s classpath", gradle.toString(), config.getAbsolutePath().toString())
+  val classpathCommand = Runtime.getRuntime().exec(gradleCommand, null, directory.toFile())
+
+  val stdout = classpathCommand.getInputStream()
+  val reader = stdout.bufferedReader()
+
+  classpathCommand.waitFor()
+
+  val artifact = Pattern.compile("^.+?\\.jar$");
+  val dependencies = mutableSetOf<Path>()
+  for (dependency in reader.lines()) {
+    val line = dependency.toString().trim()
+
+    if (artifact.matcher(line).matches()) {
+      dependencies.add(Paths.get(line));
+    }
+  }
+
+  return dependencies
 }
 
 private fun readDependenciesViaEclipseProject(connection: ProjectConnection): Set<Path> {
