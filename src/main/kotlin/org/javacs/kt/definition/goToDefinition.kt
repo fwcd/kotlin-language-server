fbe96e58ed59310e307a1c59ba231cd0366d7d0c
fwcd <fwcdmail@gmail.com>
2018-06-23 23:53:27 +0200

    Bytecode support for 'Go to definition' - fixes #23

--- src/main/kotlin/org/javacs/kt/definition/goToDefinition.kt
+++ src/main/kotlin/org/javacs/kt/definition/goToDefinition.kt
@@ -4,10 +4,63 @@ import org.eclipse.lsp4j.Location
 import org.javacs.kt.CompiledFile
 import org.javacs.kt.position.location
 import org.javacs.kt.LOG
+import org.javacs.kt.util.KotlinLSException
+import java.util.jar.JarFile
+import java.net.URI
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.io.File
 
 fun goToDefinition(file: CompiledFile, cursor: Int): Location? {
     val (_, target) = file.referenceAtPoint(cursor) ?: return null
     // TODO go to declaration name rather than beginning of javadoc comment
     LOG.info("Found declaration descriptor $target")
-    return location(target)
+    val destination = location(target)
+
+    if (destination != null) {
+        val rawURI = destination.uri
+        if (isInsideJar(rawURI)) {
+            destination.uri = readCompiledClassToTemporaryFile(rawURI)
+        }
+    }
+
+    return destination
 }
+
+private fun isInsideJar(uri: String) = uri.contains(".jar!")
+
+private fun readCompiledClassToTemporaryFile(uriWithJar: String): String {
+    val splittedUri = uriWithJar.split(".jar!")
+    val jarUri = splittedUri[0] + ".jar"
+    val jarPath = Paths.get(URI.create(jarUri))
+    val classInJarPath = Paths.get(trimLeadingPathSeparator(splittedUri[1]))
+    val className = classInJarPath.fileName.toString().trimSuffixIfPresent(".class")
+
+    JarFile(jarPath.toFile()).use { jarFile ->
+        for (jarEntry in jarFile.entries()) {
+            val jarEntryPath = Paths.get(jarEntry.name)
+
+            if (classInJarPath.equals(jarEntryPath)) {
+                // Found the correct class inside of the JAR file
+                val tmp = Files.createTempFile(className, ".class").toFile()
+                tmp.deleteOnExit() // Make sure the file is deleted upon exit
+
+                tmp.outputStream().use {
+                    jarFile.getInputStream(jarEntry).copyTo(it)
+                }
+
+                return tmp.toURI().toString()
+            }
+        }
+    }
+
+    throw KotlinLSException("Could not find $classInJarPath in ${jarPath.fileName}")
+}
+
+private fun trimLeadingPathSeparator(path: String): String {
+    val firstChar = path[0]
+    return if (firstChar == '/' || firstChar == '\\') path.substring(1) else path
+}
+
+private fun String.trimSuffixIfPresent(suffix: String) =
+        if (endsWith(suffix)) substring(0, length - suffix.length) else this
