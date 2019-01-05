15f0d93b29130e962ab19c23e152eb307ee4b64b
fwcd <fwcdmail@gmail.com>
2019-01-05 16:00:14 +0100

    Moved TeamCity URL to gradle.properties and added experimental update_kotlin_version.py script

---
+++ scripts/update_kotlin_version.py
@@ -0,0 +1,9 @@
+class TeamCityConnection:
+    def __init__(self, server_url, username, password):
+        
+
+def main():
+    conn = TeamCityConnection("https://teamcity.jetbrains.com", "guest", "guest")
+
+if __name__ == "__main__":
+    main()
