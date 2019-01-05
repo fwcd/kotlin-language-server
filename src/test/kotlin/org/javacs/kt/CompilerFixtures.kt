8cb0a990f31eab79f8655ca111011be317ff78ee
fwcd <fwcdmail@gmail.com>
2019-01-05 23:30:07 +0100

    Updated PackagePartProvider import to JvmPackagePartProvider

--- src/test/kotlin/org/javacs/kt/CompilerFixtures.kt
+++ src/test/kotlin/org/javacs/kt/CompilerFixtures.kt
@@ -3,9 +3,9 @@ package org.javacs.kt
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.cli.jvm.compiler.JvmPackagePartProvider
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.container.ComponentProvider
-import org.jetbrains.kotlin.descriptors.PackagePartProvider
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingTrace
 import org.jetbrains.kotlin.resolve.lazy.declarations.FileBasedDeclarationProviderFactory
@@ -16,7 +16,7 @@ object CompilerFixtures {
             files: Collection<KtFile>,
             trace: BindingTrace,
             configuration: CompilerConfiguration,
-            packagePartProvider: (GlobalSearchScope) -> PackagePartProvider): ComponentProvider {
+            packagePartProvider: (GlobalSearchScope) -> JvmPackagePartProvider): ComponentProvider {
         return TopDownAnalyzerFacadeForJVM.createContainer(
                 project,
                 emptyList(),
