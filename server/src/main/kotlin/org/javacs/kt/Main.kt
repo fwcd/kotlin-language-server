3b78ed175ac5dacb14d7a5eb566d897dd8bc2f81
fwcd <fwcdmail@gmail.com>
2019-08-06 14:42:16 +0200

    Remove unused Guava dependency

--- server/src/main/kotlin/org/javacs/kt/Main.kt
+++ server/src/main/kotlin/org/javacs/kt/Main.kt
@@ -9,7 +9,7 @@ import org.javacs.kt.util.ExitingInputStream
 fun main(args: Array<String>) {
     // Redirect java.util.logging calls (e.g. from LSP4J)
     LOG.connectJULFrontend()
-    
+
     val server = KotlinLanguageServer()
     val input = ExitingInputStream(System.`in`)
     val threads = Executors.newSingleThreadExecutor { Thread(it, "client") }
