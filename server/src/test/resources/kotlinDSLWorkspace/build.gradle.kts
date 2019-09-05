3dd14ba55077ab1e55f0333e052af3173001133f
fwcd <fwcdmail@gmail.com>
2019-09-05 15:26:17 +0200

    Add Gradle DSL unit test
    Verify that hovers and code completion work correctly in *.gradle.kts
    build files.

---
+++ server/src/test/resources/kotlinDSLWorkspace/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    // TODO: Currently not possible, see https://github.com/gradle/gradle/issues/9830
+    // kotlin("jvm") version "$kotlinVersion"
+    kotlin("jvm") version "1.3.50"
+}
+
+repositories {
+    jcenter()
+}
