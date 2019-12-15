05a5f3aa2f16054c5cbcb93d31b41432bd3aee41
fwcd <fwcdmail@gmail.com>
2019-12-15 02:36:56 +0100

    Fix Docker image name in deployment script

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login docker.pkg.github.com -u fwcd -p "$GH_TOKEN"
 docker build -t docker.pkg.github.com/fwcd/kotlin-language-server:latest .
-docker push fwcd/kotlin-language-server:latest
+docker push docker.pkg.github.com/fwcd/kotlin-language-server:latest
