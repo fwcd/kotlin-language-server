e8824f53b97ac28d2575561d17298f99fa2962f6
fwcd <fwcdmail@gmail.com>
2020-12-27 20:00:33 +0100

    Migrate CI scripts to GitHub actions

--- scripts/maven-deploy.sh
+++ scripts/maven-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./gradlew :shared:publish :server:publish -x test -Pgpr.user=fwcd -Pgpr.key="$GH_TOKEN"
+./gradlew :shared:publish :server:publish -x test -Pgpr.user=fwcd -Pgpr.key="$GITHUB_TOKEN"
