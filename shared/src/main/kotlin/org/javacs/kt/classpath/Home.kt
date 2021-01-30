f1fb9854299b4fdb0b0554b4a5c9138ec8867aed
David Phillips <daphillips19@gmail.com>
2021-01-30 10:38:27 -0500

    Refactor gradleHome to check for GRADLE_USER_HOME first

--- shared/src/main/kotlin/org/javacs/kt/classpath/Home.kt
+++ shared/src/main/kotlin/org/javacs/kt/classpath/Home.kt
@@ -1,6 +1,8 @@
 package org.javacs.kt.classpath
 
 import org.javacs.kt.util.userHome
+import java.nio.file.Paths
 
-internal val gradleHome = userHome.resolve(".gradle")
+internal val gradleHome = System.getenv("GRADLE_USER_HOME")?.let { Paths.get(it) } ?: userHome.resolve(".gradle")
+// TODO: try and figure out if mavenHome is in non-default position (requires finding and parsing settings.xml)
 internal val mavenHome = userHome.resolve(".m2")
