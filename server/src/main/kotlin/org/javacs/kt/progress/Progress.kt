03a1a931629ff0791f32f3cf63a80e1c31cd3f4e
fwcd <fwcdmail@gmail.com>
2021-02-27 04:32:00 +0100

    Add Progress interface

--- server/src/main/kotlin/org/javacs/kt/progress/Progress.kt
+++ server/src/main/kotlin/org/javacs/kt/progress/Progress.kt
@@ -1,5 +1,21 @@
 package org.javacs.kt.progress
 
-interface Progress {
+import java.io.Closeable
+
+/** A facility for emitting progress notifications. */
+interface Progress : Closeable {
+    /**
+     * Updates the progress percentage. The
+     * value should be in the range [0, 100].
+     */
     fun update(percent: Int): Unit
+
+    interface Factory {
+        /**
+         * Creates a new progress listener with
+         * the given label. The label is intended
+         * to be human-readable.
+         */
+        fun create(label: String): Progress
+    }
 }
