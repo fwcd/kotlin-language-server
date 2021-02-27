275115f1720f5d719b96561d101f13d0967d6aae
fwcd <fwcdmail@gmail.com>
2021-02-27 04:42:12 +0100

    Implement LanguageClientProgress

--- server/src/main/kotlin/org/javacs/kt/progress/LanguageClientProgress.kt
+++ server/src/main/kotlin/org/javacs/kt/progress/LanguageClientProgress.kt
@@ -1,2 +1,37 @@
 package org.javacs.kt.progress
 
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.jsonrpc.messages.Either
+import org.eclipse.lsp4j.ProgressParams
+import org.eclipse.lsp4j.WorkDoneProgressNotification
+import org.eclipse.lsp4j.WorkDoneProgressBegin
+import org.eclipse.lsp4j.WorkDoneProgressReport
+import org.eclipse.lsp4j.WorkDoneProgressEnd
+
+class LanguageClientProgress(
+    private val label: String,
+    private val token: Either<String, Number>,
+    private val client: LanguageClient
+) : Progress {
+    init {
+        reportProgress(WorkDoneProgressBegin().also {
+            it.title = "Kotlin: $label"
+            it.percentage = 0
+        })
+    }
+
+    override fun update(message: String?, percent: Int?) {
+        reportProgress(WorkDoneProgressReport().also {
+            it.message = message
+            it.percentage = percent
+        })
+    }
+
+    override fun close() {
+        reportProgress(WorkDoneProgressEnd())
+    }
+
+    private fun reportProgress(notification: WorkDoneProgressNotification) {
+        client.notifyProgress(ProgressParams(token, notification))
+    }
+}
--- server/src/main/kotlin/org/javacs/kt/progress/Progress.kt
+++ server/src/main/kotlin/org/javacs/kt/progress/Progress.kt
@@ -1,6 +1,7 @@
 package org.javacs.kt.progress
 
 import java.io.Closeable
+import java.util.concurrent.CompletableFuture
 
 /** A facility for emitting progress notifications. */
 interface Progress : Closeable {
@@ -8,7 +9,7 @@ interface Progress : Closeable {
      * Updates the progress percentage. The
      * value should be in the range [0, 100].
      */
-    fun update(percent: Int): Unit
+    fun update(message: String? = null, percent: Int? = null)
 
     interface Factory {
         /**
@@ -16,6 +17,6 @@ interface Progress : Closeable {
          * the given label. The label is intended
          * to be human-readable.
          */
-        fun create(label: String): Progress
+        fun create(label: String): CompletableFuture<Progress>
     }
 }
