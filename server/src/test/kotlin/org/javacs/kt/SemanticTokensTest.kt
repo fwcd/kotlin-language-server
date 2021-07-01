0b9c2134e49095ae9e388dcfcc33b3d7c3fa6108
fwcd <fwcdmail@gmail.com>
2021-07-01 19:58:21 +0200

    Tokenize entire file in test

--- server/src/test/kotlin/org/javacs/kt/SemanticTokensTest.kt
+++ server/src/test/kotlin/org/javacs/kt/SemanticTokensTest.kt
@@ -13,16 +13,17 @@ class SemanticTokensTest : SingleFileTestFixture("semantictokens", "SemanticToke
         val response = languageServer.textDocumentService.semanticTokensFull(semanticTokensParams(file)).get()!!
         val actual = response.data
         val expected = encodeTokens(sequenceOf(
-            // SemanticToken(range(1, 5, 1, 13), SemanticTokenType.PROPERTY, setOf(SemanticTokenModifier.DECLARATION)), // variable
-            // SemanticToken(range(2, 5, 2, 13), SemanticTokenType.PROPERTY, setOf(SemanticTokenModifier.DECLARATION, SemanticTokenModifier.READONLY)), // constant
-            // SemanticToken(range(2, 15, 2, 21), SemanticTokenType.CLASS), // String
-            // SemanticToken(range(2, 24, 2, 40), SemanticTokenType.STRING), // "test $variable"
-            // SemanticToken(range(2, 30, 2, 39), SemanticTokenType.INTERPOLATION_ENTRY), // $variable
-            // SemanticToken(range(2, 31, 2, 39), SemanticTokenType.VARIABLE), // variable
+            SemanticToken(range(1, 5, 1, 13), SemanticTokenType.PROPERTY, setOf(SemanticTokenModifier.DECLARATION)), // variable
 
-            // SemanticToken(range(4, 12, 4, 16), SemanticTokenType.CLASS, setOf(SemanticTokenModifier.DECLARATION)), // Type
-            // SemanticToken(range(4, 21, 4, 29), SemanticTokenType.PROPERTY, setOf(SemanticTokenModifier.DECLARATION, SemanticTokenModifier.READONLY)), // property
-            // SemanticToken(range(4, 31, 4, 34), SemanticTokenType.CLASS), // Int
+            SemanticToken(range(2, 5, 2, 13), SemanticTokenType.PROPERTY, setOf(SemanticTokenModifier.DECLARATION, SemanticTokenModifier.READONLY)), // constant
+            SemanticToken(range(2, 15, 2, 21), SemanticTokenType.CLASS), // String
+            SemanticToken(range(2, 24, 2, 40), SemanticTokenType.STRING), // "test $variable"
+            SemanticToken(range(2, 30, 2, 39), SemanticTokenType.INTERPOLATION_ENTRY), // $variable
+            SemanticToken(range(2, 31, 2, 39), SemanticTokenType.PROPERTY), // variable
+
+            SemanticToken(range(4, 12, 4, 16), SemanticTokenType.CLASS, setOf(SemanticTokenModifier.DECLARATION)), // Type
+            SemanticToken(range(4, 21, 4, 29), SemanticTokenType.PARAMETER, setOf(SemanticTokenModifier.DECLARATION, SemanticTokenModifier.READONLY)), // property
+            SemanticToken(range(4, 31, 4, 34), SemanticTokenType.CLASS), // Int
 
             SemanticToken(range(6, 5, 6, 6), SemanticTokenType.FUNCTION, setOf(SemanticTokenModifier.DECLARATION)), // f
             SemanticToken(range(6, 7, 6, 8), SemanticTokenType.PARAMETER, setOf(SemanticTokenModifier.DECLARATION, SemanticTokenModifier.READONLY)), // x
@@ -31,9 +32,6 @@ class SemanticTokensTest : SingleFileTestFixture("semantictokens", "SemanticToke
             SemanticToken(range(6, 30, 6, 31), SemanticTokenType.FUNCTION), // f
             SemanticToken(range(6, 32, 6, 33), SemanticTokenType.VARIABLE, setOf(SemanticTokenModifier.READONLY)), // x
         ))
-
-        println(actual)
-        println(expected)
 
         assertThat(actual, contains(*expected.toTypedArray()))
     }
--- server/src/test/resources/semantictokens/SemanticTokens.kt
+++ server/src/test/resources/semantictokens/SemanticTokens.kt
@@ -1,6 +1,6 @@
-// var variable = 3
-// val constant: String = "test $variable"
+var variable = 3
+val constant: String = "test $variable"
 
-// data class Type(val property: Int)
+data class Type(val property: Int)
 
 fun f(x: Int? = null): Int = f(x)
