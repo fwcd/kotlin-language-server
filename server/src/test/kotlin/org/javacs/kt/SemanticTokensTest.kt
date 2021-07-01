8b432d3a7e9d75c161dad142a124658fee36422c
fwcd <fwcdmail@gmail.com>
2021-07-01 18:40:53 +0200

    Stub out a test for semantic tokens

---
+++ server/src/test/kotlin/org/javacs/kt/SemanticTokensTest.kt
@@ -0,0 +1,9 @@
+package org.javacs.kt
+
+import org.hamcrest.Matchers.*
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class SemanticTokensTest : SingleFileTestFixture("semantictokens", "SemanticTokens.kt") {
+    // TODO
+}
