5a7e8f027b40d374fb87eb93a2c3e131d52a8c1f
rami3l <rami3l@outlook.com>
2022-02-19 13:34:04 +0100

    Fix `formatKotlinCode` defaults to match `ktfmt` presets
    (cherry picked from commit 16675dd9ed079a9795e32b23dde450177e8394d1)

--- server/src/main/kotlin/org/javacs/kt/formatting/Formatter.kt
+++ server/src/main/kotlin/org/javacs/kt/formatting/Formatter.kt
@@ -8,7 +8,9 @@ fun formatKotlinCode(
     code: String,
     options: FormattingOptions = FormattingOptions(4, true)
 ): String = Formatter.format(KtfmtOptions(
+    // If option.tabSize is 2 or 4, the generated KtfmtOptions instance concides with one of the two presets in
+    // https://github.com/facebookincubator/ktfmt/blob/d4718f643abd0999ba502caf5062c98a3218e88d/core/src/main/java/com/facebook/ktfmt/format/Formatter.kt#L45-L50
     style = KtfmtOptions.Style.GOOGLE,
     blockIndent = options.tabSize,
-    continuationIndent = 2 * options.tabSize
+    continuationIndent = options.tabSize
 ), code)
