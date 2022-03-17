db5ba9f18460a7a6c4125520b97a08cc85d5ff33
fwcd <fwcdmail@gmail.com>
2022-03-17 16:33:57 +0100

    Move vscode-java-kotlin to its own repository
    See https://github.com/fwcd/vscode-java-kotlin

--- vscode-java-kotlin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -fr jars
-mkdir -p jars
-
-cd jdt-ls-extension
-mvn clean package
-cp org.javacs.kt.jdt.ls.extension/target/org.javacs.kt.jdt.ls.extension-1.0.0-SNAPSHOT.jar ../jars/jdt-ls-extension.jar
--- vscode-java-kotlin/src/extension.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import * as vscode from 'vscode';
-
-/** Called when extension is activated */
-export async function activate(context: vscode.ExtensionContext) {
-}
