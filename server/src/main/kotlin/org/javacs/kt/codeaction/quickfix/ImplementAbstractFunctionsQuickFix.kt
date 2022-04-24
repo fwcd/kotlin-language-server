b89ec14e3b55475e2337101f74a2936dd67c5887
Marie Katrine Ekeberg <mke@themkat.net>
2022-04-24 15:10:45 +0200

    WIP. Import abstract method quick fix, external methods as well.
    External methods include Java standard library, Kotlin standard
    library and more. Some additional cleanup is needed.

--- server/src/main/kotlin/org/javacs/kt/codeaction/quickfix/ImplementAbstractFunctionsQuickFix.kt
+++ server/src/main/kotlin/org/javacs/kt/codeaction/quickfix/ImplementAbstractFunctionsQuickFix.kt
@@ -7,16 +7,24 @@ import org.javacs.kt.index.SymbolIndex
 import org.javacs.kt.position.offset
 import org.javacs.kt.position.position
 import org.javacs.kt.util.toPath
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.isInterface
+import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtSuperTypeListEntry
+import org.jetbrains.kotlin.psi.KtTypeArgumentList
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.isAbstract
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.kotlin.resolve.diagnostics.Diagnostics
+import org.jetbrains.kotlin.types.TypeProjection
+import org.jetbrains.kotlin.types.typeUtil.asTypeProjection
 
 private const val DEFAULT_TAB_SIZE = 4
 
@@ -70,8 +78,13 @@ private fun getAbstractFunctionStubs(file: CompiledFile, kotlinClass: KtClass) =
     // For each of the super types used by this class
     kotlinClass.superTypeListEntries.mapNotNull {
         // Find the definition of this super type
-        val descriptor = file.referenceAtPoint(it.startOffset)?.second
+        val referenceAtPoint = file.referenceExpressionAtPoint(it.startOffset)
+        val descriptor = referenceAtPoint?.second
         val superClass = descriptor?.findPsi()
+
+        val classDescriptor = descriptor as? ClassDescriptor
+        val superClassTypeArguments = getSuperClassTypeArguments(file, it)        
+        
         // If the super class is abstract or an interface
         if (superClass is KtClass && (superClass.isAbstract() || superClass.isInterface())) {
             // Get the abstract functions of this super type that are currently not implemented by this class
@@ -80,11 +93,27 @@ private fun getAbstractFunctionStubs(file: CompiledFile, kotlinClass: KtClass) =
             }
             // Get stubs for each function
             abstractFunctions.map { function -> getFunctionStub(function as KtNamedFunction) }
+        } else if(null != classDescriptor && (classDescriptor.kind.isInterface || classDescriptor.modality == Modality.ABSTRACT)) {
+            // TODO: refactor and prettify
+            classDescriptor.getMemberScope(superClassTypeArguments).getContributedDescriptors().filter { classMember ->
+                classMember is FunctionDescriptor && classMember.modality == Modality.ABSTRACT && !overridesDeclaration(kotlinClass, classMember)
+            }.map { function ->        
+                createFunctionStub(function as FunctionDescriptor)
+            }
         } else {
             null
         }
     }.flatten()
 
+// TODO: better name?
+private fun getSuperClassTypeArguments(file: CompiledFile, superType: KtSuperTypeListEntry): List<TypeProjection> = superType.typeReference?.typeElement?.children?.filter {
+    it is KtTypeArgumentList
+}?.flatMap {
+    (it as KtTypeArgumentList).arguments
+}?.mapNotNull {
+    (file.referenceExpressionAtPoint(it?.startOffset ?: 0)?.second as? ClassDescriptor)?.defaultType?.asTypeProjection()
+} ?: emptyList()
+    
 private fun isAbstractFunction(declaration: KtDeclaration): Boolean =
     declaration is KtNamedFunction && !declaration.hasBody()
         && (declaration.containingClass()?.isInterface() ?: false || declaration.hasModifier(KtTokens.ABSTRACT_KEYWORD))
@@ -95,6 +124,19 @@ private fun overridesDeclaration(kotlinClass: KtClass, declaration: KtDeclaratio
         if (it.name == declaration.name && it.hasModifier(KtTokens.OVERRIDE_KEYWORD)) {
             if (it is KtNamedFunction && declaration is KtNamedFunction) {
                 parametersMatch(it, declaration)
+            } else {
+                true
+            }
+        } else {
+            false
+        }
+    }
+
+private fun overridesDeclaration(kotlinClass: KtClass, descriptor: FunctionDescriptor): Boolean =
+    kotlinClass.declarations.any {
+        if (it.name == descriptor.name.asString() && it.hasModifier(KtTokens.OVERRIDE_KEYWORD)) {
+            if (it is KtNamedFunction) {
+                parametersMatch(it, descriptor)
             } else {
                 true
             }
@@ -128,8 +170,48 @@ private fun parametersMatch(function: KtNamedFunction, functionDeclaration: KtNa
     return false
 }
 
+private fun parametersMatch(function: KtNamedFunction, functionDescriptor: FunctionDescriptor): Boolean {
+    if (function.valueParameters.size == functionDescriptor.valueParameters.size) {
+        for (index in 0 until function.valueParameters.size) {
+            if (function.valueParameters[index].name != functionDescriptor.valueParameters[index].name.asString()) {
+                return false
+            } else if (function.valueParameters[index].typeReference?.name != functionDescriptor.valueParameters[index].type.toString()) {
+                // TODO: here we have exactly the old issue of type as above
+                return false
+            }
+        }
+
+        if (function.typeParameters.size == functionDescriptor.typeParameters.size) {
+            for (index in 0 until function.typeParameters.size) {
+                if (function.typeParameters[index].variance != functionDescriptor.typeParameters[index].variance) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    return false
+}
+
 private fun getFunctionStub(function: KtNamedFunction): String =
     "override fun" + function.text.substringAfter("fun") + " { }"
+
+private fun createFunctionStub(function: FunctionDescriptor): String {
+    // TODO: clean
+    val name = function.name
+    val arguments = function.valueParameters.map { argument ->
+        val argumentName = argument.name
+        // TODO: how to check if we actually should unwrap and make non-nullable?
+        val argumentType = argument.type.unwrap().makeNullableAsSpecified(false)
+                    
+        "$argumentName: $argumentType"
+    }.joinToString(", ")
+    val returnType = function.returnType?.toString()
+    
+    return "override fun $name($arguments)${returnType?.takeIf { "Unit" != it }?.let { ": $it" } ?: ""} { }"
+}
 
 private fun getDeclarationPadding(file: CompiledFile, kotlinClass: KtClass): String {
     // If the class is not empty, the amount of padding is the same as the one in the last declaration of the class
--- server/src/test/kotlin/org/javacs/kt/CodeActionTest.kt
+++ server/src/test/kotlin/org/javacs/kt/CodeActionTest.kt
@@ -72,4 +72,53 @@ class ImplementAbstractMembersQuickFixSameFileTest : SingleFileTestFixture("code
 }
 
 
+// TODO: naming
+class ImplementAbstractMembersQuickFixExternalLibraryTest : SingleFileTestFixture("codeactions", "implementabstract_standardlib.kt") {
+    @Test
+    fun `should find one abstract method from Runnable to implement`() {
+        val only = listOf(CodeActionKind.QuickFix)
+        val codeActionParams = codeActionParams(file, 5, 1, 5, 15, diagnostics, only)
+    
+        val codeActionResult = languageServer.textDocumentService.codeAction(codeActionParams).get()
 
+        assertThat(codeActionResult, hasSize(1))
+        val codeAction = codeActionResult[0].right
+        assertThat(codeAction.kind, equalTo(CodeActionKind.QuickFix))
+        assertThat(codeAction.title, equalTo("Implement abstract functions"))
+        //assertThat(codeAction.diagnostics, equalTo(listOf(diagnostics[0])))
+
+        val textEdit = codeAction.edit.changes
+        val key = workspaceRoot.resolve(file).toUri().toString()
+        assertThat(textEdit.containsKey(key), equalTo(true))
+        assertThat(textEdit[key], hasSize(1))
+
+        val functionToImplementEdit = textEdit[key]?.get(0)
+        assertThat(functionToImplementEdit?.range, equalTo(range(5, 28, 5, 28)))
+        assertThat(functionToImplementEdit?.newText, equalTo("\n\n    override fun run() { }"))
+    }
+
+    @Test
+    fun `should find one abstract method from Comparable to implement`() {
+        val only = listOf(CodeActionKind.QuickFix)
+        val codeActionParams = codeActionParams(file, 7, 1, 7, 19, diagnostics, only)
+    
+        val codeActionResult = languageServer.textDocumentService.codeAction(codeActionParams).get()
+
+        assertThat(codeActionResult, hasSize(1))
+        val codeAction = codeActionResult[0].right
+        assertThat(codeAction.kind, equalTo(CodeActionKind.QuickFix))
+        assertThat(codeAction.title, equalTo("Implement abstract functions"))
+        //assertThat(codeAction.diagnostics, equalTo(listOf(diagnostics[0])))
+
+        val textEdit = codeAction.edit.changes
+        val key = workspaceRoot.resolve(file).toUri().toString()
+        assertThat(textEdit.containsKey(key), equalTo(true))
+        assertThat(textEdit[key], hasSize(1))
+
+        val functionToImplementEdit = textEdit[key]?.get(0)
+        assertThat(functionToImplementEdit?.range, equalTo(range(7, 42, 7, 42)))
+        assertThat(functionToImplementEdit?.newText, equalTo("\n\n    override fun compare(p0: String, p1: String): Int { }"))
+    }
+}
+
+// TODO: should we have tests for one method already being in place? and then trying to implement the rest?
