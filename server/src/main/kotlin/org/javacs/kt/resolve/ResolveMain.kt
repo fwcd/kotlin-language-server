29f0a9fdf7075f07642d2e49ed000e5ca6f2d2e0
fwcd <fwcdmail@gmail.com>
2022-06-08 23:10:40 +0200

    Reformat ResolveMain with some smaller stylistic changes

--- server/src/main/kotlin/org/javacs/kt/resolve/ResolveMain.kt
+++ server/src/main/kotlin/org/javacs/kt/resolve/ResolveMain.kt
@@ -10,27 +10,26 @@ import org.javacs.kt.position.range
 import org.javacs.kt.util.partitionAroundLast
 import com.intellij.openapi.util.TextRange
 
-
 fun resolveMain(file: CompiledFile): Map<String,Any> {
     val parsedFile = file.parse.copy() as KtFile
-    
-    val mainFunction = findTopLevelMainFunction(parsedFile)
-    if(null != mainFunction) {
+
+    findTopLevelMainFunction(parsedFile)?.let { mainFunction ->
         // the KtFiles name is weird. Full path. This causes the class to have full path in name as well. Correcting to top level only
         parsedFile.name = parsedFile.name.partitionAroundLast("/").second.substring(1)
 
-        return mapOf("mainClass" to JvmFileClassUtil.getFileClassInfoNoResolve(parsedFile).facadeClassFqName.asString(),
-                     "range" to range(file.content, mainFunction.second))
+        return mapOf(
+            "mainClass" to JvmFileClassUtil.getFileClassInfoNoResolve(parsedFile).facadeClassFqName.asString(),
+            "range" to range(file.content, mainFunction.second)
+        )
     }
 
-    val companionMain = findCompanionObjectMain(parsedFile)
-    if(null != companionMain) {
+    findCompanionObjectMain(parsedFile)?.let { companionMain ->
         return mapOf(
             "mainClass" to (companionMain.first ?: ""),
             "range" to range(file.content, companionMain.second)
         )
     }
-    
+
     return emptyMap()
 }
 
@@ -42,21 +41,25 @@ private fun findTopLevelMainFunction(file: KtFile): Pair<String?, TextRange>? = 
 }
 
 // finds a top level class that contains a companion object with a main function inside
-private fun findCompanionObjectMain(file: KtFile): Pair<String?, TextRange>? = file.declarations.flatMap { topLevelDeclaration ->
-    if(topLevelDeclaration is KtClass) {
-        topLevelDeclaration.companionObjects
-    } else {
-        emptyList<KtObjectDeclaration>()
+private fun findCompanionObjectMain(file: KtFile): Pair<String?, TextRange>? = file.declarations
+    .flatMap { topLevelDeclaration ->
+        if (topLevelDeclaration is KtClass) {
+            topLevelDeclaration.companionObjects
+        } else {
+            emptyList<KtObjectDeclaration>()
+        }
     }
-}.flatMap {    companionObject ->
-    companionObject.body?.children?.toList() ?: emptyList()
-}.mapNotNull {  companionObjectInternal ->
-    if(companionObjectInternal is KtNamedFunction && "main" == companionObjectInternal.name && companionObjectInternal.text.startsWith("@JvmStatic")) {
-        companionObjectInternal
-    } else {
-        null
+    .flatMap { companionObject ->
+        companionObject.body?.children?.toList() ?: emptyList()
     }
-}.firstOrNull()?.let {
-    // a little ugly, but because of success of the above, we know that "it" has 4 layers of parent objects (child of companion object body, companion object body, companion object, outer class)
-    Pair((it.parent.parent.parent.parent as KtClass).fqName?.toString(), it.textRange)
-}
+    .mapNotNull { companionObjectInternal ->
+        companionObjectInternal.takeIf {
+            companionObjectInternal is KtNamedFunction
+            && "main" == companionObjectInternal.name
+            && companionObjectInternal.text.startsWith("@JvmStatic")
+        }
+    }
+    .firstOrNull()?.let {
+        // a little ugly, but because of success of the above, we know that "it" has 4 layers of parent objects (child of companion object body, companion object body, companion object, outer class)
+        Pair((it.parent.parent.parent.parent as KtClass).fqName?.toString(), it.textRange)
+    }
