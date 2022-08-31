a3c040e297602b8cc705c327f8a79713a0fbe17d
Marie Katrine Ekeberg <mke@themkat.net>
2022-08-31 17:49:17 +0200

    Change private helper function into an extension function

--- server/src/main/kotlin/org/javacs/kt/codeaction/quickfix/QuickFix.kt
+++ server/src/main/kotlin/org/javacs/kt/codeaction/quickfix/QuickFix.kt
@@ -7,6 +7,7 @@ import org.eclipse.lsp4j.Range
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.javacs.kt.CompiledFile
 import org.javacs.kt.index.SymbolIndex
+import org.javacs.kt.util.isSubrangeOf
 import org.jetbrains.kotlin.resolve.diagnostics.Diagnostics
 import org.jetbrains.kotlin.diagnostics.Diagnostic as KotlinDiagnostic
 
@@ -16,15 +17,7 @@ interface QuickFix {
 }
 
 fun diagnosticMatch(diagnostic: Diagnostic, range: Range, diagnosticTypes: Set<String>): Boolean =
-    isDiagnosticInRange(diagnostic, range) && diagnosticTypes.contains(diagnostic.code.left)
-
-// for a diagnostic to be in range the lines should be the same, and
-//  the input character range should be within the bounds of the diagnostics range.
-private fun isDiagnosticInRange(diagnostic: Diagnostic, range: Range): Boolean {
-    val diagnosticRange = diagnostic.range
-    return diagnosticRange.start.line == range.start.line && diagnosticRange.end.line == range.end.line &&
-        diagnosticRange.start.character <= range.start.character && diagnosticRange.end.character >= range.end.character
-}
+    range.isSubrangeOf(diagnostic.range) && diagnosticTypes.contains(diagnostic.code.left)
 
 fun diagnosticMatch(diagnostic: KotlinDiagnostic, startCursor: Int, endCursor: Int, diagnosticTypes: Set<String>): Boolean =
     diagnostic.textRanges.any { it.startOffset <= startCursor && it.endOffset >= endCursor } && diagnosticTypes.contains(diagnostic.factory.name)
