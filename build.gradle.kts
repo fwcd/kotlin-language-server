ecc3f09e2673c57fa913d4f01d0ecda569fa2ebb
Henry Fraser <henrymfraser@gmail.com>
2023-02-18 17:40:33 +1000

    Add Detekt to the build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // TODO: change the hardcoded 1.6.10 to kotlinVeresion that lives in project props
 // We could use a buildSrc script for this.
 // See: https://github.com/gradle/kotlin-dsl-samples/issues/802
 plugins {
     kotlin("jvm") version "1.8.10"
     `maven-publish`
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 repositories {
@@ -33,3 +36,24 @@ configure(subprojects.filter { it.name != "grammars" }) {
         }
     }
 }
+
+detekt {
+    allRules = false // activate all available (even unstable) rules.
+    buildUponDefaultConfig = true // preconfigure defaults
+    parallel = true
+    config = files("$rootDir/detekt.yml")
+    source = files(projectDir)
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        md.required.set(true)
+    }
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "11"
+}
+
+tasks.check.get().dependsOn(tasks.detekt)
