91a29b28f32804f62f68cbc2600ebfd5ee5a0f46
fwcd <fwcdmail@gmail.com>
2023-04-14 18:12:17 +0200

    Search for .bat etc only on Windows

--- shared/src/main/kotlin/org/javacs/kt/classpath/ShellClassPathResolver.kt
+++ shared/src/main/kotlin/org/javacs/kt/classpath/ShellClassPathResolver.kt
@@ -5,6 +5,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import org.javacs.kt.util.userHome
+import org.javacs.kt.util.isOSWindows
 import org.javacs.kt.LOG
 
 /** Executes a shell script to determine the classpath */
@@ -31,7 +32,7 @@ internal class ShellClassPathResolver(
     companion object {
         private val configDirNames = listOf("kotlin-language-server", "KotlinLanguageServer")
         private val scriptNames = listOf("kls-classpath", "kotlinLspClasspath")
-        private val scriptExtensions = listOf("", ".sh", ".bat", ".cmd")
+        private val scriptExtensions = if (isOSWindows()) listOf(".bat", ".cmd", ".ps1") else listOf("", ".sh", ".bash")
 
         /** Create a shell resolver if a file is a pom. */
         fun maybeCreate(file: Path): ShellClassPathResolver? =
