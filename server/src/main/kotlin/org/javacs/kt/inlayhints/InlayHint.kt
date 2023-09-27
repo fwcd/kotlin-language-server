da7e78625b2c820274f57e9678ddea0a4347691f
Elam Cohavi <elam.cohavi@gmail.com>
2023-09-27 20:37:39 +0100

    include lambda hints

--- server/src/main/kotlin/org/javacs/kt/inlayhints/InlayHint.kt
+++ server/src/main/kotlin/org/javacs/kt/inlayhints/InlayHint.kt
@@ -6,9 +6,9 @@ import org.eclipse.lsp4j.InlayHint
 import org.eclipse.lsp4j.InlayHintKind
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.javacs.kt.CompiledFile
+import org.javacs.kt.completion.DECL_RENDERER
 import org.javacs.kt.position.range
 import org.javacs.kt.util.preOrderTraversal
-import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.model.ArgumentMatch
@@ -17,29 +17,31 @@ import org.jetbrains.kotlin.resolve.calls.util.getParentResolvedCall
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.error.ErrorType
 
-private fun PsiElement?.determineType(ctx: BindingContext): KotlinType? =
-    this?.let {
-        when (this) {
-            is KtDestructuringDeclarationEntry -> {
-                val resolvedCall = ctx[BindingContext.COMPONENT_RESOLVED_CALL, this]
-                resolvedCall?.resultingDescriptor?.returnType
-            }
-            is KtProperty -> {
-                //TODO: better handling for unresolved-type error
-                val type = this.getKotlinTypeForComparison(ctx)
-                if (type is ErrorType) null else type
-            }
-
-            else -> null
+private fun PsiElement.determineType(ctx: BindingContext): KotlinType? =
+    when (this) {
+        is KtDestructuringDeclarationEntry -> {
+            val resolvedCall = ctx[BindingContext.COMPONENT_RESOLVED_CALL, this]
+            resolvedCall?.resultingDescriptor?.returnType
         }
+        is KtProperty -> {
+            //TODO: better handling for unresolved-type error
+            val type = this.getKotlinTypeForComparison(ctx)
+            if (type is ErrorType) null else type
+        }
+
+        else -> null
     }
 
-private fun PsiElement.hintBuilder(kind: InlayHintKind, file: CompiledFile, arg: Name? = null): InlayHint {
+private fun PsiElement.hintBuilder(kind: InlayHintKind, file: CompiledFile, hintLabel: String? = null): InlayHint? {
     val namedElement = ((this as? PsiNameIdentifierOwner)?.nameIdentifier ?: this)
     val range = range(file.parse.text, namedElement.textRange)
     val (pos, label) = when(kind) {
-        InlayHintKind.Type -> Pair(range.end, ": ${this.determineType(file.compile)}")
-        InlayHintKind.Parameter -> Pair(range.start, "$arg:")
+        InlayHintKind.Type -> {
+            this.determineType(file.compile) ?.let {
+                Pair(range.end, ": ${DECL_RENDERER.renderType(it)}")
+            } ?: return null
+        }
+        InlayHintKind.Parameter -> Pair(range.start, "$hintLabel:")
     }
     val hint = InlayHint(pos, Either.forLeft(label))
     hint.kind = kind
@@ -52,10 +54,10 @@ private fun valueArgsToHints(
     callExpression: KtCallExpression,
     file: CompiledFile,
 ): List<InlayHint> {
-    return callExpression.valueArguments.map {
+    return callExpression.valueArguments.mapNotNull {
         val call = it.getParentResolvedCall(file.compile)
         val arg = (call?.getArgumentMapping(it) as ArgumentMatch).valueParameter.name
-        it.hintBuilder(InlayHintKind.Parameter, file, arg)
+        it.hintBuilder(InlayHintKind.Parameter, file, arg.asString())
     }
 }
 
@@ -68,12 +70,12 @@ fun provideHints(file: CompiledFile): List<InlayHint> {
                 hints.addAll(valueArgsToHints(node, file))
             }
             is KtDestructuringDeclaration -> {
-                hints.addAll(node.entries.map {  it.hintBuilder(InlayHintKind.Type, file) })
+                hints.addAll(node.entries.mapNotNull {  it.hintBuilder(InlayHintKind.Type, file) })
             }
             is KtProperty -> {
                 //check decleration does not include type i.e. var t1: String
                 if (node.typeReference == null) {
-                    hints.add(node.hintBuilder(InlayHintKind.Type, file))
+                    node.hintBuilder(InlayHintKind.Type, file)?.let { hints.add(it) }
                 }
             }
         }
