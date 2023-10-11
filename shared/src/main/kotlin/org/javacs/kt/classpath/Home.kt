e4cdef7c62cea3f6e1a21dac361fa402c364a111
Callum Lamont <callum.lamont@complyadvantage.com>
2023-10-11 20:53:47 +0100

    Fix maven repository discovery
    Fixes the maven repository discovery by checking if the candidate paths
    have a `/repository` directory. An exception is thrown if no valid
    repository paths are found.

--- shared/src/main/kotlin/org/javacs/kt/classpath/Home.kt
+++ shared/src/main/kotlin/org/javacs/kt/classpath/Home.kt
@@ -1,18 +1,28 @@
 package org.javacs.kt.classpath
 
-import org.javacs.kt.util.userHome
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import org.javacs.kt.util.KotlinLSException
+import org.javacs.kt.util.userHome
 
+private fun createPathOrNull(envVar: String): Path? {
+  return System.getenv(envVar)?.let { Paths.get(it) }
+}
 
-internal val gradleHome =
-    System.getenv("GRADLE_USER_HOME")?.let { Paths.get(it) }
-        ?: userHome.resolve(".gradle")
-
-internal val mavenHome =
-    System.getenv("MAVEN_HOME")?.let { Paths.get(it) }
-        ?: System.getenv("M2_HOME")?.let { Paths.get(it) }
-        ?: userHome.resolve(".m2")
+private val possibleMavenRepositoryPaths =
+    sequenceOf(
+            createPathOrNull("MAVEN_REPOSITORY"),
+            createPathOrNull("MAVEN_HOME")?.let { it.resolve("repository") },
+            createPathOrNull("M2_HOME")?.let { it.resolve("repository") },
+            userHome.resolve(".m2/repository"),
+        )
+        .filterNotNull()
 
 internal val mavenRepository =
-    System.getenv("MAVEN_REPOSITORY")?.let { Paths.get(it) }
-        ?: mavenHome.resolve("repository")
+    possibleMavenRepositoryPaths.firstOrNull { Files.exists(it) }
+        ?: throw KotlinLSException(
+            "No repositories found at \$MAVEN_REPOSITORY, \$MAVEN_HOME, \$M2_HOME or \$HOME/.m2"
+        )
+
+internal val gradleHome = createPathOrNull("GRADLE_USER_HOME") ?: userHome.resolve(".gradle")
