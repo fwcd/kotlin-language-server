77d109a97053f6dd9318bdae7047df77e55a2033
Elam Cohavi <elam.cohavi@gmail.com>
2023-10-13 11:46:16 +0100

    refactor fix

--- server/src/main/kotlin/org/javacs/kt/inlayhints/InlayHint.kt
+++ server/src/main/kotlin/org/javacs/kt/inlayhints/InlayHint.kt
@@ -7,6 +7,7 @@ import org.eclipse.lsp4j.InlayHint
 import org.eclipse.lsp4j.InlayHintKind
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.javacs.kt.CompiledFile
+import org.javacs.kt.LOG
 import org.javacs.kt.completion.DECL_RENDERER
 import org.javacs.kt.position.range
 import org.javacs.kt.util.preOrderTraversal
@@ -80,7 +81,7 @@ private fun callableArgsToHints(
     val entries = resolvedCall?.valueArguments?.entries ?: return emptyList()
 
     return entries.mapNotNull { (t, u) ->
-        val valueArg = u.arguments.singleOrNull()
+        val valueArg = u.arguments.firstOrNull()
         if (valueArg != null && !valueArg.isNamed()) {
             val label = getLabel(t.name, u)
             valueArg.asElement().hintBuilder(InlayKind.ParameterHint, file, label)
