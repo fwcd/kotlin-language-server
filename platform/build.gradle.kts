1fd3dc4e9b9cff130c66e24a36c00b478bd9d4a9
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-12-24 17:14:23 +0900

    feat: use catalog to manager dependencies

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -1,42 +1,30 @@
-plugins {
-    id("java-platform")
-}
+plugins { id("java-platform") }
 
-javaPlatform {
-    allowDependencies()
-}
+javaPlatform { allowDependencies() }
 
-val kotlinVersion = "1.9.20"
-val exposedVersion = "0.37.3"
-val lsp4jVersion = "0.21.2"
-
-// constrain the dependencies that we use to these specific versions
 dependencies {
     constraints {
-        api("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-        api("org.hamcrest:hamcrest-all:1.3")
-        api("junit:junit:4.11")
-        api("org.eclipse.lsp4j:org.eclipse.lsp4j:$lsp4jVersion")
-        api("org.eclipse.lsp4j:org.eclipse.lsp4j.jsonrpc:$lsp4jVersion")
-        api("org.jetbrains.kotlin:kotlin-compiler:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-scripting-compiler:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-scripting-jvm-host-unshaded:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-sam-with-receiver-compiler-plugin:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-jvm:$kotlinVersion")
-        api("org.jetbrains:fernflower:1.0")
-        api("org.jetbrains.exposed:exposed-core:$exposedVersion")
-        api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-        api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-        api("com.h2database:h2:1.4.200")
-        api("com.github.fwcd.ktfmt:ktfmt:b5d31d1")
-        api("com.beust:jcommander:1.78")
-        api("org.hamcrest:hamcrest-all:1.3")
-        api("junit:junit:4.11")
-        api("org.openjdk.jmh:jmh-core:1.20")
-        api("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
-        api("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
-        api("org.openjdk.jmh:jmh-generator-annprocess:1.20")
-        api("org.xerial:sqlite-jdbc:3.41.2.1")
+        api(libs.org.jetbrains.kotlin.stdlib)
+        api(libs.hamcrest.all)
+        api(libs.junit.junit)
+        api(libs.org.eclipse.lsp4j.lsp4j)
+        api(libs.org.eclipse.lsp4j.jsonrpc)
+        api(libs.org.jetbrains.kotlin.compiler)
+        api(libs.org.jetbrains.kotlin.ktscompiler)
+        api(libs.org.jetbrains.kotlin.kts.jvm.host.unshaded)
+        api(libs.org.jetbrains.kotlin.sam.with.receiver.compiler.plugin)
+        api(libs.org.jetbrains.kotlin.reflect)
+        api(libs.org.jetbrains.kotlin.jvm)
+        api(libs.org.jetbrains.fernflower)
+        api(libs.org.jetbrains.exposed.core)
+        api(libs.org.jetbrains.exposed.dao)
+        api(libs.org.jetbrains.exposed.jdbc)
+        api(libs.com.h2database.h2)
+        api(libs.com.github.fwcd.ktfmt)
+        api(libs.com.beust.jcommander)
+        api(libs.org.openjdk.jmh.core)
+        api(libs.org.jetbrains.kotlin.kotlin.scripting.jvm.host)
+        api(libs.org.openjdk.jmh.generator.annprocess)
+        api(libs.org.xerial.sqlite.jdbc)
     }
 }
