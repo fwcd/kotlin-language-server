4c51f6370b8786408f3583342155b82db37c73d0
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-12-24 19:20:15 +0900

    feat: to 2.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
     }
-    plugins {
-        kotlin("jvm") version "2.1.0" apply false
-    }
 }
 
 rootProject.name = "kotlin-language-server"
